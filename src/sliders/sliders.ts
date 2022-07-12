8a47c83d3af4819da1c7251ea26ed33b0952df2d
Danya <danilomaster2003@ukr.net>
2022-07-12 23:20:36 +0300

    feat: add UI

---
+++ src/sliders/sliders.ts
@@ -0,0 +1,35 @@
+import * as noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
+
+let slider_quantity: noUiSlider.target = document.getElementById('slider_quantity')! as noUiSlider.target;
+let slider_year: noUiSlider.target = document.getElementById('slider_year')! as noUiSlider.target;
+
+noUiSlider.create(slider_quantity, {
+    start: [15, 30],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 5,
+        'max': 50
+    },
+});
+
+noUiSlider.create(slider_year, {
+    start: [2019, 2021],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 2016,
+        'max': 2022
+    },
+});
+
+slider_quantity.noUiSlider?.on('update', function(values){
+    document.getElementById('formatting-start-quantity')!.textContent = String(Math.floor(values[0] as number));
+    document.getElementById('formatting-end-quantity')!.textContent = String(Math.floor(values[1] as number));
+});
+
+slider_year.noUiSlider?.on('update', function(values){
+    document.getElementById('formatting-start-year')!.textContent = String(Math.floor(values[0] as number));
+    document.getElementById('formatting-end-year')!.textContent = String(Math.floor(values[1] as number));
+});
