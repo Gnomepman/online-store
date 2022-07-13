251cef3c57dd60aba3474b6ca56f711257854ffe
Danya <danilomaster2003@ukr.net>
2022-07-13 23:13:56 +0300

    feat: add array of phones

---
+++ src/array of phones/phones.ts
@@ -0,0 +1,27 @@
+import { Phone } from '../phone/Phone'
+
+export let phones: Phone[] = [
+    new Phone("../assets/galaxy_a14_32gb.png", 'samsung', "Galaxy a14", 2020, 'light', 3, true, 20),
+    new Phone("../assets/galaxy_a32_123gb.png", 'samsung', "Galaxy a32 128GB", 2021, 'light', 3, false, 15),
+    new Phone("../assets/galaxy_a32_63gb.png", 'samsung', "Galaxy a32 63GB", 2021, "dark", 3, false, 20),
+    new Phone("../assets/galaxy_a52_128gb.png", 'samsung', "Galaxy a52", 2020, 'light', 3, false, 10),
+    new Phone("../assets/galaxy_a73_256gb.png", 'samsung', 'Galaxy a73', 2022, 'light', 3, true, 40),
+    new Phone("../assets/galaxy_M32.png", 'samsung', "Galaxy m32", 2021, 'dark', 1, false, 15),
+    new Phone("../assets/galaxy_m33_5g.png", 'samsung', "Galaxy m33", 2020, 'dark', 3, false, 50),
+    new Phone("../assets/galaxy_m53_5g.png", 'samsung', "Galaxy m53", 2021, 'dark', 3, true, 27),
+    new Phone("../assets/galaxy_s21_256gb.png", 'samsung', "Galaxy s21", 2020, 'dark', 3, false, 14),
+    new Phone("../assets/iphone_11.png", "apple", 'Iphone 11', 2019, 'light', 2, true, 19),
+    new Phone("../assets/iphone_12_64_gb.png", 'apple', 'Iphone 12', 2020, 'blue', 2, false, 8),
+    new Phone("../assets/iphone_13_mini.png", "apple", "Iphone 13 mini", 2021, 'light', 2, false, 35),
+    new Phone("../assets/iphone_13_pro_1tb.png", 'apple', "Iphone 13 pro", 2021, 'blue', 3, true, 31),
+    new Phone("../assets/iphone_13_pro_max_512_gb.png", 'apple', "Iphone 13 pro max", 2021, 'light', 3, false, 24),
+    new Phone("../assets/samsung_a53_5g.png", 'samsung', 'a53', 2016, 'dark', 3, false, 14),
+    new Phone('../assets/xiaomi_poco_x4_pro_256_gb.png', 'xiaomi', 'Poco x4 pro', 2018, 'light', 2, false, 46),
+    new Phone("../assets/xiaomi_redmi_9_64gb.png", 'xiaomi', 'Redmi 9' , 2018, 'blue', 3, true, 6),
+    new Phone("../assets/xiaomi_redmi_9a.png", 'xiaomi', 'Redmi 9a 128GB', 2016, 'blue', 1, false, 34),
+    new Phone('../assets/xiaomi_redmi_9A_64_gb.png', "xiaomi", "Redmi 9a 64gb", 2016, 'blue', 1, false, 34),
+    new Phone('../assets/xiaomi_redmi_9c.png', 'xiaomi', "Redmi 9c", 2017, 'dark', 1, false, 28),
+    new Phone("../assets/xiaomi_redmi_note_9_128_gb.png", "xiaomi", "Redmi note 9", 2021, "blue", 3, false, 31),
+    new Phone("../assets/xiaomi_redmi_note_11_128_gb.png", 'xiaomi', "Redmi note 11", 2022, 'dark', 3, true, 14),
+    new Phone("../assets/xiaomi_redmi_note_11_pro_128_gb.png", 'xiaomi', "Redmi note 11 pro", 2022, 'dark', 3, false, 44)
+];
