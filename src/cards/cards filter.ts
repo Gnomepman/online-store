d1388061d42fdea928442a00fea903afb4038ce4
Danya <danilomaster2003@ukr.net>
2022-07-17 22:17:48 +0300

    feat: function for filtering cards

---
+++ src/cards/cards filter.ts
@@ -0,0 +1,52 @@
+import { user_options } from "../types and interfaces/interfaces";
+import { manufactures } from "../types and interfaces/interfaces";
+import { colors } from "../types and interfaces/interfaces";
+import { cameras } from "../types and interfaces/interfaces";
+
+export function filter_cards(settings: user_options = JSON.parse(localStorage.user_settings) as user_options){
+    const cards = document.getElementById("cards")!.children;
+    for (const element of cards){
+        dispay(element);
+        if(settings.manufacture.length > 0 && !(settings.manufacture.includes(element.querySelector("#card_name")!.textContent as manufactures))){
+          hide(element);
+        }
+        if(
+          settings.number_of_cameras.length > 0 &&
+          !(settings.number_of_cameras.includes(element.querySelector("#card_cameras")!.textContent!.split(" ")[1] as unknown as cameras) as unknown as cameras)
+        ) {
+          hide(element);
+        }
+        if(settings.color.length > 0 && !(settings.color.includes(element.querySelector("#card_color")!.textContent as colors))){
+          hide(element);
+        }
+        if(settings.popularity && (element.querySelector("#card_popularity")!.textContent !== 'популярный')){
+          hide(element);
+        }
+        if(Number(element.querySelector("#card_quantity")!.textContent?.split(" ")[2]) < Number(settings.range_storage[0]) ||
+        Number(element.querySelector("#card_quantity")!.textContent?.split(" ")[2]) > Number(settings.range_storage[1])
+        ){
+          hide(element);
+        }
+        if(Number(element.querySelector("#card_year")!.textContent) < Number(settings.range_year[0]) ||
+        Number(element.querySelector("#card_year")!.textContent) > Number(settings.range_year[1])
+        ){
+          hide(element);
+        }
+    }
+    //sort_cards()
+}
+
+/*
+Татьяна, если вы смотрите на это и думаете: "Ведь это бред, можно было использовать один класс filtered{dispay: none}"
+И метод .toggle() - то у меня почему-то не перезаписывались стили. Уверен, этому есть разумное объяснение, но я его не нашел
+*/
+function hide(element: Element) {
+  element.classList.remove("flex");
+  element.classList.add("filtered");
+}
+
+function dispay(element: Element) {
+  element.classList.remove("filtered");
+  element.classList.add("flex");
+}
+
